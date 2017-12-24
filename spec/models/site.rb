96f0b24cb7a0ae987d4ce719b0805f0202201600
sweetymehta <smehta1289@gmail.com>
2017-12-24 12:56:55 +0530

    Revert "test"
    This reverts commit 7991b73f5c87236cead9a804455a0861e5e5c579.

--- spec/models/site.rb
+++ spec/models/site.rb
@@ -3,11 +3,7 @@ class Site
 
   field :name
 
-  if Mongoid::Compatibility::Version.mongoid3_or_older?
-    has_many :users, inverse_of: :site, order: :email.desc
-  else
-    has_many :users, inverse_of: :site, order: :email.desc, counter_cache: true
-  end  
+  has_many :users, inverse_of: :site, order: :email.desc, counter_cache: true
 
   validates :name, presence: true, uniqueness: true
 end
