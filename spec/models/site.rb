7991b73f5c87236cead9a804455a0861e5e5c579
sweetymehta <smehta1289@gmail.com>
2017-12-24 12:46:50 +0530

    test

--- spec/models/site.rb
+++ spec/models/site.rb
@@ -3,7 +3,11 @@ class Site
 
   field :name
 
-  has_many :users, inverse_of: :site, order: :email.desc, counter_cache: true
+  if Mongoid::Compatibility::Version.mongoid3_or_older?
+    has_many :users, inverse_of: :site, order: :email.desc
+  else
+    has_many :users, inverse_of: :site, order: :email.desc, counter_cache: true
+  end  
 
   validates :name, presence: true, uniqueness: true
 end
