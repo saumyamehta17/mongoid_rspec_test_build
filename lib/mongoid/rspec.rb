563f506dcbc9affe9f70daf2a8e7a2316fc9603b
sweetymehta <smehta1289@gmail.com>
2017-12-29 21:04:42 +0530

    test

--- lib/mongoid/rspec.rb
+++ lib/mongoid/rspec.rb
@@ -29,8 +29,11 @@ require 'matchers/be_mongoid_document'
 require 'matchers/be_dynamic_document'
 require 'matchers/be_stored_in'
 require 'matchers/have_field'
-require 'matchers/have_index_for'
-# require 'matchers/indexes'
+if Mongoid::Compatibility::Version.mongoid4_or_newer?
+  require 'matchers/have_index_for'
+else
+  require 'matchers/indexes'
+end  
 require 'matchers/have_timestamps'
 
 module Mongoid
