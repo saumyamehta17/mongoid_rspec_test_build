723e0d7193112be21925f39aa68d8859b5bfb7a3
sweetymehta <smehta1289@gmail.com>
2017-12-29 17:23:38 +0530

    test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,7 @@ require 'mongoid-rspec'
 RSpec.configure do |config|
   config.include RSpec::Matchers
   config.include Mongoid::Matchers
+  config.include Mongoid3::Matchers
   config.mock_with :rspec
   config.after :all do
     Mongoid::Config.purge!
