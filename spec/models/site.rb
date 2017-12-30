34627504209f9440bc60562d68b41a77b022c21b
sweetymehta <smehta1289@gmail.com>
2017-12-30 20:05:15 +0530

    removed version mongoid 7 and head

--- spec/models/site.rb
+++ spec/models/site.rb
@@ -3,11 +3,7 @@ class Site
 
   field :name
 
-  if Mongoid::Compatibility::Version.mongoid7_or_newer?
-    has_many :users, inverse_of: :site, order: :email.desc
-  else
-    has_many :users, inverse_of: :site, order: :email.desc, counter_cache: true
-  end  
+  has_many :users, inverse_of: :site, order: :email.desc, counter_cache: true
 
   validates :name, presence: true, uniqueness: true
 end
