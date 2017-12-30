a6c14ab919adec12985cf6c33f9844c46e2a5cfb
sweetymehta <smehta1289@gmail.com>
2017-12-30 19:07:33 +0530

    counter cache fix for v 7

--- spec/models/site.rb
+++ spec/models/site.rb
@@ -3,10 +3,10 @@ class Site
 
   field :name
 
-  if Mongoid::Compatibility::Version.mongoid3_or_newer?
-    has_many :users, inverse_of: :site, order: :email.desc, counter_cache: true
-  else
+  if Mongoid::Compatibility::Version.mongoid7_or_newer?
     has_many :users, inverse_of: :site, order: :email.desc
+  else
+    has_many :users, inverse_of: :site, order: :email.desc, counter_cache: true
   end  
 
   validates :name, presence: true, uniqueness: true
