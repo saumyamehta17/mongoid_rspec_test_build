9d07f398c84c3aec6affcfb153ff5490b85ab212
sweetymehta <smehta1289@gmail.com>
2018-01-01 18:57:45 +0530

    fail test

--- lib/matchers/indexes/v3/have_index_for.rb
+++ lib/matchers/indexes/v3/have_index_for.rb
@@ -1,8 +1,8 @@
 module Mongoid
   module Matchers
 
-    def have_index_for(index_fields)
-      HaveIndexFor.new(index_fields)
+    def have_index_for(index_key)
+      HaveIndexFor.new(index_key)
     end
 
     class HaveIndexFor < Mongoid::Matchers::Base::HaveIndexFor
@@ -13,27 +13,27 @@ module Mongoid
 
         if @klass.respond_to?(:index_options)
           # Mongoid 3
-          unless @klass.index_options[@index_fields]
-            @errors.push "no index for #{@index_fields}"
+          unless @klass.index_options[@index_key]
+            @errors.push "no index for #{@index_key}"
           else
-            if !@options.nil? && !@options.empty?
-              @options.each do |option, option_value|
-                if denormalising_options(@klass.index_options[@index_fields])[option] != option_value
-                  @errors.push "index for #{@index_fields.inspect} with options of #{@klass.index_options[@index_fields].inspect}"
+            if !@index_options.nil? && !@index_options.empty?
+              @index_options.each do |option, option_value|
+                if denormalising_options(@klass.index_options[@index_key])[option] != option_value
+                  @errors.push "index for #{@index_key.inspect} with options of #{@klass.index_options[@index_key].inspect}"
                 end
               end
             end
           end
         else
           # Mongoid 4
-          unless @klass.index_specifications.map(&:key).include?(@index_fields)
-            @errors.push "no index for #{@index_fields}"
+          unless @klass.index_specifications.map(&:key).include?(@index_key)
+            @errors.push "no index for #{@index_key}"
           else
-            if !@options.nil? && !@options.empty?
-              index_options = @klass.index_specifications.select { |is| is.key == @index_fields }.first.options
-              @options.each do |option, option_value|
+            if !@index_options.nil? && !@index_options.empty?
+              index_options = @klass.index_specifications.select { |is| is.key == @index_key }.first.options
+              @index_options.each do |option, option_value|
                 if index_options[option] != option_value
-                  @errors.push "index for #{@index_fields.inspect} with options of #{index_options.inspect}"
+                  @errors.push "index for #{@index_key.inspect} with options of #{index_options.inspect}"
                 end
               end
             end
@@ -55,8 +55,8 @@ module Mongoid
       alias :failure_message_when_negated :failure_message_for_should_not
 
       def description
-        desc = "have an index for #{@index_fields.inspect}"
-        desc << " with options of #{@options.inspect}" if @options
+        desc = "have an index for #{@index_key.inspect}"
+        desc << " with options of #{@index_options.inspect}" if @index_options
         desc
       end
 
